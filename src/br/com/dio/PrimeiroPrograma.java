f92b3056329c9a25a240e7563d8c47eab23fc125
Cristiano Reis <cristianobr@live.com>
2023-02-05 19:33:52 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+      /*  System.out.println("Hello wolrd");
+        int a  = 5;
+        int b = 6;
+        System.out.println("Hello world  " +  (a+b) );*/
+        Gato  gato = new Gato();
+        System.out.println(gato);
+
+    }
+}
